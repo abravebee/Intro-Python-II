351100883b4e3983075377d1cf5fe6196eefac26
Casey Bee <caseye.baker@gmail.com>
2019-04-24 16:09:39 -0700

    better foundations now that I'm passing the whole room object and not just the key name

--- src/adv.py
+++ src/adv.py
@@ -45,19 +45,77 @@ player = Player("Jane", room["outside"])
 #
 
 
-
 while True:
 # * Prints the current room name
+# * Prints the current description (the textwrap module might be useful here).
+
     print((
         '\n You are here: {}'
-        '\n {}'
+        '\n {}\n'
         ).format(
             player.current_room.name,
-            player.current_room.description
+            player.current_room.desc
         ))
-# * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
-#
+    userin = input((
+        "What would you like to do?\n"
+        "> ")).lower()
+    
+    if "help" in userin or userin == 'h':
+        print((
+        '\nHELP MENU'
+        '\n N > Go North, S > Go South, E > Go East, W > Go West'
+        '\n H > Help Menu'
+        '\n Q > Quit'
+        ))
+        continue
+
+    if "north" in userin or userin == 'n':
+        try:
+            player.current_room = player.current_room.n_to
+            print("You went north.")
+        except:
+            print("You can't go that way.")
+        continue
+    
+    if "south" in userin or userin == 's':
+        try:
+            player.current_room = player.current_room.s_to
+            print("You went south.")
+        except:
+            print("You can't go that way.")
+        continue
+
+    if "east" in userin or userin == 'e':
+        try:
+            player.current_room = player.current_room.e_to
+            print("You went east.")
+        except:
+            print("You can't go that way.")
+        continue
+
+    if "west" in userin or userin == 'w':
+        try:
+            player.current_room = player.current_room.w_to
+            print("You went west.")
+        except:
+            print("You can't go that way.")
+        continue
+
+    if "quit" in userin or userin == 'q':
+        quit_check = input((
+            '\nReally quit?  y/n'
+            '\n> '
+        )).lower()
+        if quit_check == 'y':
+            print("See ya!")
+            break
+        elif quit_check == 'n':
+            print("Returning to game...")
+            continue
+
+    else:
+        print("userin", userin)
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
