41be3a6feb09300cdc3f73a7bebec1f9832c251c
Casey Bee <caseye.baker@gmail.com>
2019-04-24 15:33:00 -0700

    I was really overcomplicating this, trying the loop again

--- src/adv.py
+++ src/adv.py
@@ -40,57 +40,25 @@ room["treasure"].s_to = room["narrow"]
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player("Jane", "outside")
-rm_current = room[player.current_room]
-rm_name = room[player.current_room].name
-rm_desc = room[player.current_room].desc
+player = Player("Jane", room["outside"])
 # Write a loop that:
 #
-# * Prints the current room name
-print(rm_name)
-# * Prints the current description (the textwrap module might be useful here).
-print(rm_desc)
 
+
+
+while True:
+# * Prints the current room name
+    print((
+        '\n You are here: {}'
+        '\n {}'
+        ).format(
+            player.current_room.name,
+            player.current_room.description
+        ))
+# * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 #
-user_in = input(
-    ("What would you like to do? \n" "Enter a cardinal direction or Q to quit.\n")
-)
 # If the user enters a cardinal direction, attempt to move to the room there.
-
-while not user_in == "Q" and not user_in == "q":
-    if "north" in user_in or "North" in user_in:
-        try:
-            print("You go North")
-            player.current_room = rm_current.n_to.name
-            print(player.current_room)
-        except:
-            print("You can't go that way.")
-    elif "south" in user_in or "South" in user_in:
-        try:
-            print("You go South")
-
-        except:
-            print("You can't go that way.")
-    elif "east" in user_in or "East" in user_in:
-        try:
-            print("You go East")
-
-        except:
-            print("You can't go that way.")
-    elif "west" in user_in or "West" in user_in:
-        try:
-            print("You go West")
-
-        except:
-            print("You can't go that way.")
-    else:
-        print("I don't understand.")
-    #print(room[player.current_room].name)
-    user_in = input(
-        ("What would you like to do? \n" "Enter a cardinal direction or Q to quit.\n")
-    )
-
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
