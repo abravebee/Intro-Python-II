e1d5dd3ec0fce264211d206e578b4fce9348fd39
Casey Bee <caseye.baker@gmail.com>
2019-04-24 12:56:26 -0700

    add_item method for Room class and container for items

--- src/room.py
+++ src/room.py
@@ -5,9 +5,13 @@ class Room:
     def __init__(self, name, desc):
         self.name = name
         self.desc = desc
+        self.contains = []
     
     def get_name(self):
         return self.name
 
     def get_description(self):
         return self.desc
+
+    def add_item(self, item):
+        self.contains.append(item)
